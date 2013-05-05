80a370badef0d50eb4b322fb3462aefde680f5c3
Hanno Schlichting <hanno@hannosch.eu>
2013-05-05 09:37:39 +0200

    use a .rst ending

--- setup.py
+++ setup.py
@@ -15,6 +15,18 @@
 from os.path import join
 from setuptools import setup, find_packages, Extension
 
+README = open('README.rst').read()
+CHANGES = open('CHANGES.rst').read()
+
+ext_modules = [
+    Extension(
+        name='Missing._Missing',
+        include_dirs=['include', 'src'],
+        sources=[join('src', 'Missing', '_Missing.c')],
+        depends=[join('include', 'ExtensionClass', 'ExtensionClass.h')]),
+]
+
+
 setup(
     name='Missing',
     version='2.13.2dev',
@@ -23,16 +35,10 @@ setup(
     description="Special Missing objects used in Zope2.",
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
-    long_description=open('README.txt').read() + '\n' +
-        open('CHANGES.txt').read(),
+    long_description='\n\n'.join([README, CHANGES]),
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    ext_modules=[Extension(
-        name='Missing._Missing',
-        include_dirs=['include', 'src'],
-        sources=[join('src', 'Missing', '_Missing.c')],
-        depends=[join('include', 'ExtensionClass', 'ExtensionClass.h')]),
-    ],
+    ext_modules=ext_modules,
     install_requires=['ExtensionClass'],
     include_package_data=True,
     zip_safe=False,
