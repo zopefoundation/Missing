02b27da9299b060dc21139189ec6868a5ca7e086
Hanno Schlichting <hanno@hannosch.eu>
2013-05-05 09:35:17 +0200

    pep8

--- setup.py
+++ setup.py
@@ -15,25 +15,25 @@
 from os.path import join
 from setuptools import setup, find_packages, Extension
 
-setup(name='Missing',
-      version = '2.13.2dev',
-      url='http://pypi.python.org/pypi/Missing',
-      license='ZPL 2.1',
-      description="Special Missing objects used in Zope2.",
-      author='Zope Foundation and Contributors',
-      author_email='zope-dev@zope.org',
-      long_description=open('README.txt').read() + '\n' +
-                       open('CHANGES.txt').read(),
-
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      ext_modules=[Extension(
-            name='Missing._Missing',
-            include_dirs=['include', 'src'],
-            sources=[join('src', 'Missing', '_Missing.c')],
-            depends=[join('include', 'ExtensionClass', 'ExtensionClass.h')]),
-      ],
-      install_requires=['ExtensionClass'],
-      include_package_data=True,
-      zip_safe=False,
-      )
+setup(
+    name='Missing',
+    version='2.13.2dev',
+    url='http://pypi.python.org/pypi/Missing',
+    license='ZPL 2.1',
+    description="Special Missing objects used in Zope2.",
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description=open('README.txt').read() + '\n' +
+        open('CHANGES.txt').read(),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    ext_modules=[Extension(
+        name='Missing._Missing',
+        include_dirs=['include', 'src'],
+        sources=[join('src', 'Missing', '_Missing.c')],
+        depends=[join('include', 'ExtensionClass', 'ExtensionClass.h')]),
+    ],
+    install_requires=['ExtensionClass'],
+    include_package_data=True,
+    zip_safe=False,
+)
