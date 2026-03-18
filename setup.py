7fccbad76ee7884c9cbfa16cf20e732a4173251d
Michael Howitz <icemac@gmx.net>
2026-03-18 08:41:39 +0100

    Move package metadata from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -11,53 +11,8 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-
-from setuptools import find_packages
 from setuptools import setup
 
 
-README = open('README.rst').read()
-CHANGES = open('CHANGES.rst').read()
-
-version = '5.1.dev0'
-
-setup(
-    name='Missing',
-    version=version,
-    url='https://github.com/zopefoundation/Missing',
-    project_urls={
-        'Issue Tracker': 'https://github.com/zopefoundation/Missing/issues',
-        'Sources': 'https://github.com/zopefoundation/Missing',
-    },
-    license='ZPL-2.1',
-    description="Special Missing objects used in Zope.",
-    author='Zope Foundation and Contributors',
-    author_email='zope-dev@zope.dev',
-    long_description='\n\n'.join([README, CHANGES]),
-    keywords="zope missing object",
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    classifiers=[
-        "Development Status :: 6 - Mature",
-        "Environment :: Web Environment",
-        "Framework :: Zope :: 5",
-        "License :: OSI Approved :: Zope Public License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-    ],
-    python_requires='>=3.10',
-    install_requires=[
-        'ExtensionClass >= 4.1a1',
-        'zope.deferredimport',
-    ],
-    include_package_data=True,
-    zip_safe=False,
-)
+# See pyproject.toml for package metadata
+setup()
